bab2cc15194b12d5201bbad031fb6f675a5fb20b
mohit <mohitvohracr50@gmail.com>
2025-10-05 18:19:02 +0530

    Add new 'api' app with health endpoint and register it

--- larc_dev_api/settings.py
+++ larc_dev_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'agents',
     'subscriptions',
+    'api',
     'rest_framework',
     'oauth2_provider',
 
--- larc_dev_api/urls.py
+++ larc_dev_api/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('agents/', include('agents.urls')),
     path('subscriptions/', include('subscriptions.urls')),
+    path('api/', include('api.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/',TokenVerifyView.as_view(),name='token_verify'),
