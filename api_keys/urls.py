f580827cc9236f2f0e4e7622c90bdddde65bea55
varruunnn <varun.choudhary2512@gmail.com>
2026-01-01 00:56:24 +0530

    integration phase 1 completed

--- api_keys/urls.py
+++ api_keys/urls.py
@@ -3,5 +3,5 @@ from .views import UserAPIKeyListCreateView, UserAPIKeyRevokeView
 
 urlpatterns = [
     path("api-keys/", UserAPIKeyListCreateView.as_view(), name="api_key_list_create"),
-    path("api-keys/<int:pk>/", UserAPIKeyRevokeView.as_view(), name="api_key_revoke"),
+    path("api-keys/<str:pk>/", UserAPIKeyRevokeView.as_view(), name="api_key_revoke"),
 ]
